68fb93641281a479e6998a983fb64c7d11b2dbe5
Liam-ar2 <alcantaraliam8@gmail.com>
2026-06-06 19:38:27 -0400

    Tarea 3

---
+++ Tarea 3/Loto MQTT/Loto MQTT/Apuestas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loto_MQTT
+{
+    public class ApuestaMsg
+
+    {
+        public string ClienteId { get; set; }
+        public int Numero { get; set; }
+        public double Monto { get; set; }
+    }
+}
